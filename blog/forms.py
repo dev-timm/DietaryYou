9256d3a659a749f883eda692eb62d34206f3156d
dev-timm <timm.kuemmeth@gmail.com>
2023-09-27 16:01:00 +0000

    add chevrons to form selects, fix spacing issue on the bottom and remove commented out code

--- blog/forms.py
+++ blog/forms.py
@@ -11,9 +11,9 @@ class PostForm(forms.ModelForm):
             'title': forms.TextInput(attrs={'class': 'form-control form-control-lg'}),
             'slug': forms.TextInput(attrs={'class': 'form-control form-control-lg'}),
             'featured_image': forms.FileInput(attrs={'class': 'form-control form-control-lg'}),
-            'author': forms.Select(attrs={'class': 'form-control form-control-lg'}),
+            'author': forms.Select(attrs={'class': 'form-control form-control-lg form-select'}),
             'content': forms.Textarea(attrs={'class': 'form-control form-control-lg'}),
-            'status': forms.Select(attrs={'class': 'form-control form-control-lg'}),            
+            'status': forms.Select(attrs={'class': 'form-control form-control-lg form-select'}),            
         }
 
 
@@ -26,7 +26,7 @@ class EditForm(forms.ModelForm):
             'slug': forms.TextInput(attrs={'class': 'form-control form-control-lg'}),
             'featured_image': forms.FileInput(attrs={'class': 'form-control form-control-lg'}),
             'content': forms.Textarea(attrs={'class': 'form-control form-control-lg'}),
-            'status': forms.Select(attrs={'class': 'form-control form-control-lg'}),            
+            'status': forms.Select(attrs={'class': 'form-control form-control-lg form-select'}),            
         }
 
 
