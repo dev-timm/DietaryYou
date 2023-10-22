2c9e70e57248cbe077a21a94d2c658e6bf26abba
dev-timm <timm.kuemmeth@gmail.com>
2023-10-22 10:02:53 +0000

    remigrate blog models to correct data field and fix db inconsistencies

--- blog/models.py
+++ blog/models.py
@@ -11,11 +11,11 @@ class Post(models.Model):
     title = models.CharField(max_length=100, unique=True)
     slug = models.SlugField(max_length=100, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
-    updated_on = models.DateField(auto_now=True)
+    updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
     featured_image = CloudinaryField('image', default='placeholder')
     excerpt = models.TextField(blank=True)
-    created_on = models.DateField(auto_now_add=True)
+    created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     likes = models.ManyToManyField(User, related_name="blog_likes", blank=True)
 
@@ -38,7 +38,7 @@ class Comment(models.Model):
     name = models.CharField(max_length=60)
     email = models.EmailField()
     body = models.TextField()
-    created_on = models.DateField(auto_now_add=True)
+    created_on = models.DateTimeField(auto_now_add=True)
     approved = models.BooleanField(default=False)
 
     class Meta:
