abbcefe6ec07f81b828fa1a7df3e0d8dbcadd027
dev-timm <timm.kuemmeth@gmail.com>
2023-10-22 12:20:42 +0000

    migrate profiles model

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary. models import CloudinaryField
 
-# Create your models here.
+
+class UserProfile(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_image = CloudinaryField('image', default='placeholder')
+    full_name = models.CharField(max_length=100)
+    bio = models.TextField()
+    hobbies = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ['full_name']
