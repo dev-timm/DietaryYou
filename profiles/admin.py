cf7ef576d31dac8a79166c51fba7af8b24d4a1c0
dev-timm <timm.kuemmeth@gmail.com>
2023-10-22 12:44:39 +0000

    set up admin UI for user profiles

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import UserProfile
 
-# Register your models here.
+
+@admin.register(UserProfile)
+class PostAdmin(admin.ModelAdmin):
+
+    list_display = ('user', 'full_name', 'location')
+    search_fields = ['full_name', 'bio', 'hobbies', 'location']
+    list_filter = ('full_name', 'hobbies', 'location')
