bdbc5e05f93bb0f5db89b8455c5f92716d89e839
dev-timm <timm.kuemmeth@gmail.com>
2023-10-27 09:50:31 +0000

    add public user profile pages and link to them from blog area

--- profiles/urls.py
+++ profiles/urls.py
@@ -6,4 +6,5 @@ from django.contrib.auth.decorators import login_required
 urlpatterns = [
     path('user_account', login_required(views.UserAccount.as_view()), name='user_account'),
     path('user_profile', login_required(views.ViewProfile.as_view()), name='user_profile'),
+    path('<slug:slug>/', views.ViewPublicProfile.as_view(), name='public_user_profile'),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,5 @@
+from django.shortcuts import render, get_object_or_404
+from django.views import View
 from django.views.generic.base import TemplateView
 from .models import UserProfile
 
@@ -17,3 +19,18 @@ class ViewProfile(TemplateView):
         context["user_profile_data"] = UserProfile.objects.filter(user=current_user)[0]
         
         return context
+
+
+class ViewPublicProfile(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = UserProfile.objects.all()
+        user_profile_data = get_object_or_404(queryset, slug=slug)
+
+        return render(
+            request,
+            "public_user_profile.html",
+            {
+                "user_profile_data": user_profile_data,
+            },
+        )
