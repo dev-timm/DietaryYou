81ddd2d7ad78d10cebe4d83e19083c84bff50e4e
dev-timm <timm.kuemmeth@gmail.com>
2023-10-28 14:32:20 +0000

    add user posts section to settings

--- profiles/urls.py
+++ profiles/urls.py
@@ -6,6 +6,7 @@ from django.contrib.auth.decorators import login_required
 urlpatterns = [
     path('user_account', login_required(views.UserAccount.as_view()), name='user_account'),
     path('user_profile', login_required(views.ViewProfile.as_view()), name='user_profile'),
+    path('user_posts', login_required(views.ViewUserPosts.as_view()), name='user_posts'),
     path('<slug:slug>/', views.ViewPublicProfile.as_view(), name='public_user_profile'),
     path('edit_profile/<slug:slug>/', login_required(views.EditProfile.as_view()), name='edit_profile'),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from django.views.generic.base import TemplateView
 from .models import UserProfile
+from blog.models import Post, Comment
+from django.db.models import Count, Q
 from .forms import EditProfileForm
 from django.contrib import messages
 
@@ -51,3 +53,23 @@ class EditProfile(generic.UpdateView):
             messages.add_message(self.request, messages.ERROR, "Ooops, something went wrong. Please try again!")
 
         return super().form_valid(form)
+
+
+class ViewUserPosts(TemplateView):
+
+    template_name = 'user_posts.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        current_user = self.request.user
+        approved_comments = Count('comments', filter=Q(comments__approved=True))
+        user_posts = Post.objects.filter(author=current_user).annotate(approved_comments=approved_comments).order_by('-created_on')
+        published_posts = user_posts.filter(status=1)
+        draft_posts = user_posts.filter(status=0)
+
+        context = {
+            'published_posts': published_posts,
+            'draft_posts': draft_posts,
+        }
+
+        return context
