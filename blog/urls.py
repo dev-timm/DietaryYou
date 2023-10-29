a8e22650f45a65d9c898da2c2126950a3e192ae7
dev-timm <timm.kuemmeth@gmail.com>
2023-10-29 19:28:10 +0000

    further ensure that user must be logged in when working with posts

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,12 @@
 from . import views
 from django.urls import path
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='post_list'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
-    path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
-    path('new_post', views.AddPost.as_view(), name='add_post'),
-    path('edit_post/<slug:slug>/', views.EditPost.as_view(), name='edit_post'),
-    path('delete_post/<slug:slug>/', views.DeletePost.as_view(), name='delete_post'),
+    path('like/<slug:slug>', login_required(views.PostLike.as_view()), name='post_like'),
+    path('new_post', login_required(views.AddPost.as_view()), name='add_post'),
+    path('edit_post/<slug:slug>/', login_required(views.EditPost.as_view()), name='edit_post'),
+    path('delete_post/<slug:slug>/', login_required(views.DeletePost.as_view()), name='delete_post'),
 ]
